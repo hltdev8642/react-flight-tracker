af26de67fd3c8771897465a9e4099d89de189442
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-08-20 17:02:49 +0200

    feat: use MobileEarth as suspense for Earth

--- src/components/3d/Earth.tsx
+++ src/components/3d/Earth.tsx
@@ -4,10 +4,20 @@ import EarthColorMap from "../../assets/earthmap/8081_earthmap10k.jpg";
 import EarthSpecularMap from "../../assets/earthmap/8081_earthspec10k.jpg";
 import EarthBumpMap from "../../assets/earthmap/8081_earthbump10k.jpg";
 import EarthNightMap from "../../assets/earthmap/5_night_16k.jpg";
-import {Sphere} from "@react-three/drei";
+import {Sphere, useProgress} from "@react-three/drei";
 import {EARTH_RADIUS} from "../../constants.ts";
+import {toast} from "react-toastify";
+import {useEffect} from "react";
 
 export default function Earth() {
+    const {active, progress} = useProgress()
+    if (active && progress !== 100 && !toast.isActive('loadingEarth')) {
+        toast.loading(`Downloading High Resolution Earth Textures...`, {
+            toastId: 'loadingEarth',
+            progress: progress,
+            autoClose: false,
+        })
+    }
     // load texture
     const [colorMap, specularMap, bumpMap, nightMap] = useLoader(TextureLoader, [
             EarthColorMap,
@@ -16,6 +26,16 @@ export default function Earth() {
             EarthNightMap
         ]
     ) as Texture[]
+
+
+    useEffect(() => {
+            if (!active && progress === 100) {
+                toast.dismiss('loadingEarth')
+            }
+        }
+        , [active, progress])
+
+
     return (
         <><Sphere args={[EARTH_RADIUS, 50, 50]}>
             <meshPhongMaterial specularMap={specularMap}/>
