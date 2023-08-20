8c3ed9c9bb98c5434f7a20b2c51f105c94f489d5
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-08-20 17:07:11 +0200

    feat: use low-res textures for MobileEarth instead of no textures at all

--- src/components/3d/MobileEarth.tsx
+++ src/components/3d/MobileEarth.tsx
@@ -1,14 +1,29 @@
 import {EARTH_RADIUS} from "../../constants.ts";
 import {Sphere} from "@react-three/drei";
+import {useLoader} from "@react-three/fiber";
+import {Texture, TextureLoader} from "three";
+import EarthColorMap from "../../assets/earthmap-compressed/8081_earthmap10k-min.jpg";
+import EarthNightMap from "../../assets/earthmap-compressed/5_night_16k-min.jpg";
 
 export default function MobileEarth() {
 
+    // load texture
+    const [colorMap, nightMap] = useLoader(TextureLoader, [
+            EarthColorMap,
+            EarthNightMap
+        ]
+        ,
+        undefined,
+    ) as Texture[]
 
     return (
         <>
-            <Sphere args={[EARTH_RADIUS, 50, 50]}>
+            <Sphere args={[EARTH_RADIUS, 20, 20]}>
                 <meshStandardMaterial
-                    color={'#000000'}
+                    map={colorMap}
+                    emissiveMap={nightMap}
+                    emissiveIntensity={5}
+                    emissive={0xaaaaaa}
                 />
             </Sphere>
         </>
